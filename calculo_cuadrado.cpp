93535fff251c9fc66a87b88231a8f3dec388a9ea
Kiwi-000 <antoniatadea283@gmail.com>
2024-08-16 18:46:43 -0400

    se completo programa de calcular al cuadrado

--- calculo_cuadrado.cpp
+++ calculo_cuadrado.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+int cuadrado(int x){
+    return x * x;
+}
+
+void print_cuadrado(int x){
+    std::cout << x << " al cuadrado es " << cuadrado(x) ;
+}
+
 int main(){
-    
+    print_cuadrado(2);
+
 }
