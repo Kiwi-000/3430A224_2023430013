2dcd91d4b47eb3636ade928078596c6805bfe925
Kiwi-000 <antoniatadea283@gmail.com>
2024-08-16 20:26:02 -0400

    Update laboratorio_0.cpp

--- laboratorio_0.cpp
+++ laboratorio_0.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
+#include <string>
 
 //funcion para ver si el número es par o impar
 bool par_impar(int numero){
-    return numero / 2 == 0
+    return numero % 2 == 0;
 }
 
 
-
 int main() {
-    int numero = 1;
-    //std::cout << numero ;
+
+    //Evaluar número par o impar 
+    int numero;
+
+    std::cout << "Ingrese un número: " ;
+    std::cin >> numero;
+
+    if (par_impar(numero)){
+        std::cout << "El número ingresado es par."<< std::endl;
+    }
+    else{
+        std::cout << "El número ingresado es impar."<< std::endl;
+    }
+
+    // para que no de problemas con el getline
+    std::cin.ignore();
+
+    // Texto
+    std::string texto;
+
+    std::cout << "Ingrese un texto: ";
+    std::getline(std::cin, texto);
+    std::cout << texto;
+    
 }
