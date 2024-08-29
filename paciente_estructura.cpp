9d50826f53e615cac2e26e76eda4807ea9bef872
Kiwi-000 <antoniatadea283@gmail.com>
2024-08-29 16:41:38 -0400

    Create paciente_estructura.cpp

---
+++ paciente_estructura.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Paciente{
+    string nombre;
+    int edad;
+    int peso;
+    double altura;
+};
+
+int main(){
+    
+    Paciente persona[3];
+
+    persona[0].nombre = "Camila";
+    persona[0].edad = 45;
+    persona[0].peso = 66;
+    persona[0].altura = 1.70;
+
+    persona[1].nombre = "Juan";
+    persona[1].edad = 5;
+    persona[1].peso = 15;
+    persona[1].altura = 90;
+
+    persona[2].nombre = "Ana";
+    persona[2].edad = 17;
+    persona[2].peso = 45;
+    persona[2].altura = 1.55;
+            
+            
+    for ( int i= 0; i < 3; ++i){
+        cout <<"Nombre: "<< persona[i].nombre << endl;
+        cout << "Edad: " << persona[i].edad << endl;
+        cout << "Peso: " << persona[i].peso << endl;
+        cout << "Altura: " << persona[i].altura << endl;
+        cout << " " << endl;
+
+    };
+
+    return 0;
+
+}
+
